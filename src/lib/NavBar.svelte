be952278494fbe5ef160c66f58c5519f07c0a466
Natto <zx4545zx.zx4545zx@gmail.com>
2023-02-07 14:49:21 +0700

    chang theme select

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -41,64 +41,14 @@
 
 <div class="navbar bg-primary mb-5 w-full">
   <div class="navbar-start">
-    <div class="dropdown">
-      <button class="btn btn-ghost btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-list"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-          />
-        </svg>
-      </button>
-      <ul
-        class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-      >
-        <li><a href="/">Homepage</a></li>
-      </ul>
-    </div>
-  </div>
-  <div class="navbar-center">
     <a href="/" class="btn btn-ghost normal-case text-xl">HOROSCOPE</a>
   </div>
   <div class="navbar-end">
-    <div class="dropdown dropdown-end">
-      <button class="btn btn-ghost btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-palette-fill"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M12.433 10.07C14.133 10.585 16 11.15 16 8a8 8 0 1 0-8 8c1.996 0 1.826-1.504 1.649-3.08-.124-1.101-.252-2.237.351-2.92.465-.527 1.42-.237 2.433.07zM8 5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm4.5 3a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
-          />
-        </svg>
-      </button>
-      <ul
-        class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-      >
-        <li>
-          <button data-set-theme="mytheme" data-act-class="ACTIVECLASS"
-            >default</button
-          >
-        </li>
-        {#each theme as tme}
-          <li>
-            <button data-set-theme={tme} data-act-class="ACTIVECLASS"
-              >{tme}</button
-            >
-          </li>
-        {/each}
-      </ul>
-    </div>
+    <select data-choose-theme class="select select-secondary w-full max-w-xs">
+      <option value="mytheme">Default</option>
+      {#each theme as tme}
+        <option value={tme}>{tme}</option>
+      {/each}
+    </select>
   </div>
 </div>
--- src/lib/Select.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  export let sign;
-  export let day;
-</script>
-
